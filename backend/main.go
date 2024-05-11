c733a4c926e332e0afcc2154aed9eb8e24109e1d
Soham Ghugare <soham.ghugare@gmail.com>
2024-05-11 15:24:10 +0530

    feat: added initializers

--- backend/main.go
+++ backend/main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"net/http"
 
+	"github.com/SohamGhugare/hackbangalore-24/initializers"
 	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	initializers.LoadEnv()
+}
 
 func main() {
 	r := gin.Default()
 
 	// test route
-		r.GET("/ping", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "Pong!",
-			})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Pong!",
 		})
+	})
 
 	// running the server
 	r.Run()
