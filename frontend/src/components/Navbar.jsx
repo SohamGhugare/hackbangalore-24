88ad24ea62a1f8903f2e584abc6e167d5ad376d8
Soham Ghugare <soham.ghugare@gmail.com>
2024-05-12 08:32:04 +0530

    fix: fixed filtering

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import Button from "./ui/Button";
 
 const Navbar = () => {
   return (
-    <nav className="absolute w-full flex justify-between my-4 px-8">
-      <Link href={"/"} className="font-bold">
+    <nav className="absolute w-full flex items-center justify-between my-4 px-8">
+      <Link href={"/"} className="font-bold text-2xl">
         Sustain<span className="text-green-800">X</span>
       </Link>
       <div className="flex gap-8">
@@ -12,7 +12,7 @@ const Navbar = () => {
         <Link href={"/"}>Features</Link>
         <Link href={"/"}>Profile</Link>
       </div>
-      {/* <div> 
+      {/* <div>
         <Link href={"/"}>Login</Link>
         <Button variant={"secondary"} text="Sign Up"/>
       </div> */}
--- frontend/src/components/explore-page/Filters.jsx
+++ frontend/src/components/explore-page/Filters.jsx
@@ -8,14 +8,57 @@ import {
 } from "@/components/ui/select";
 import Label from "../ui/Label";
 import { useState } from "react";
+import StartupCard from "../ui/StartupCard";
+function getRandomImage() {
+  return images[Math.floor(Math.random() * 5)];
+}
+const images = [
+  "https://cdn.angellist.com/startups/i/434782-c98e482a57fb3174015721fa6cb999f5-thumb_jpg.jpg?buster=1466584506",
+  "https://cdn.angellist.com/startups/i/787705-963e2659f2be78b2b2224364fe391761-thumb_jpg.jpg?buster=1644449912",
+  "https://cdn.angellist.com/startups/i/453027-f62871b5f6f5a5d67fe4b3005603c7d9-thumb_jpg.jpg?buster=1698993291",
+  "https://cdn.angellist.com/startups/i/677316-fa0984dace218f67a578f18c1d950dd7-thumb_jpg.jpg?buster=1583869992",
+  "https://cdn.angellist.com/startups/i/4055695-c45adb3dfae48acf65ed20c325e4d167-thumb_jpg.jpg?buster=1536344232",
+];
 
-const Filters = () => {
+const Filters = ({ setLoading, startups, setStartups }) => {
   const [data, setData] = useState({
     socialImpact: "",
     environmentImpact: "",
     riskTolerance: "",
     investmentDuration: "",
   });
+  const getData = async () => {
+    const arr = [
+      data.socialImpact,
+      data.environmentImpact,
+      data.riskTolerance,
+      data.investmentDuration,
+    ];
+    const query = arr.filter((e) => e !== "").join(",");
+    setLoading(true);
+    fetch(`http://127.0.0.1:8080/api/v1/startups?filter=${query}`)
+      .then((res) => {
+        if (res.status === 200) {
+          setLoading(false);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        const cards = data?.startups.map((startup) => (
+          <StartupCard
+            key={startup._id}
+            id={startup._id}
+            name={startup.name}
+            logo={getRandomImage()}
+            industry={startup.industry}
+            stage_of_development={startup.stage_of_development}
+          />
+        ));
+        console.log("\n\n\n\n\n\n\n\n\n\n");
+        console.log(data.startups);
+        setStartups(cards);
+      });
+  };
   return (
     <section>
       <div className="flex flex-wrap justify-center gap-10 my-10">
@@ -110,7 +153,12 @@ const Filters = () => {
         </div>
       </div>
       <div className="flex justify-center">
-        <button className="bg-[#4fc87d] py-2 px-4 rounded-lg">
+        <button
+          onClick={() => {
+            getData();
+          }}
+          className="bg-[#4fc87d] py-2 px-4 rounded-lg my-10"
+        >
           Apply Filters
         </button>
       </div>
