cf4646cf95c8a3795160f1fee3df1a51d2e60a5c
arjunbector <arjunbector@gmail.com>
2024-05-12 09:54:23 +0530

    add animation

--- frontend/src/components/landing-page/HeroSection.jsx
+++ frontend/src/components/landing-page/HeroSection.jsx
@@ -1,24 +1,70 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import hero from "../../../public/landing-page/hero.png";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
     <section className="flex items-center justify-center min-h-screen px-32">
       <div className="w-full">
-        <h1 className="text-3xl md:text-5xl font-bold">Welcome to</h1>
-        <div className="">
+        <motion.h1
+          initial={{
+            opacity: 0,
+            y: 10,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{ type: "spring", stiffness: 100 }}
+          className="text-3xl md:text-5xl font-bold"
+        >
+          Welcome to
+        </motion.h1>
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: 10,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{ type: "spring", stiffness: 100, delay: 0.2 }}
+          className="text-3xl md:text-5xl font-bold"
+        >
           <div className="relative ml-4 my-6 h-6 w-1/3 bg-[#69d18dca] ">
             <h1 className="absolute -top-5 -left-4 text-3xl pt-[-36] md:text-5xl my-2 font-bold z-[9999]">
               SustainX
             </h1>
           </div>
-        </div>
-        <p>
+        </motion.div>
+        <motion.p
+          initial={{
+            opacity: 0,
+            y: 10,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{ type: "spring", stiffness: 100, delay: 0.4 }}
+        >
           Unleash the potential of blockchain to drive positive change and fuel
           groundbreaking projects that shape a better tomorrow
-        </p>
-        <div className="flex justify-start gap-6 mt-4">
+        </motion.p>
+        <motion.div 
+        initial={{
+          opacity: 0,
+          y: 10,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{ type: "spring", stiffness: 100, delay: 0.6}}
+         className="flex justify-start gap-6 mt-4">
           <Link
             className="bg-primary-green w-40 p-2 rounded-md text-center font-semibold"
             href={"/investor/signup"}
@@ -31,7 +77,7 @@ const HeroSection = () => {
           >
             I run a Start Up
           </Link>
-        </div>
+        </motion.div>
       </div>
       <div className="w-full">
         <Image className="h-full w-auto" src={hero} alt="hero" />
